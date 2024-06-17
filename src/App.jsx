8c8124403ce4327e5efadbfe1e5b267c9c87f1b1
Grechileme <grechilemelucas@gmail.com>
2024-06-17 15:26:45 -0400

    Added function to delete button

--- src/App.jsx
+++ src/App.jsx
@@ -55,11 +55,21 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId),
+      };
+    });
+  }
+
   // console.log(projectsState.projects);
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAppProject} onCancel={handleCancelAddProject} />
